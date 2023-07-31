111da17c3dd3c7168473efac97d50878f7fefe22
Yaohong Xiang <xiang.yao@northeastern.edu>
2023-07-31 15:26:59 -0400

    add structure and frontend register page

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+
+
+export default function App(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/" element={<Chat></Chat>}></Route>
+      </Routes>
+    </BrowserRouter>
+    // <Register></Register>
+  )
+};
