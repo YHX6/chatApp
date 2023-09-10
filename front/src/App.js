160ed01ee336193d5a3157218d194369c04d682a
Yaohong Xiang <xiang.yao@northeastern.edu>
2023-09-10 11:05:30 -0400

    1

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,22 @@
-import { host } from './utils/APIRoutes';
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+// import Chat from "./pages/Chat";
+// import SetAvatar from "./pages/SetAvatar";
+import "./assets/css/style.css";
 
-function App() {
-  console.log(host);
+
+export default function App(){
   return (
-    <div className="App">
-     hello
-    </div>
-  );
-}
-
-export default App;
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        {/* <Route path="/setAvatar" element={<SetAvatar></SetAvatar>}></Route>
+        <Route path="/" element={<Chat></Chat>}></Route> */}
+      </Routes>
+    </BrowserRouter>
+    // <Register></Register>
+  )
+};
