c0519e1b2bdc9096c6fd9bf45b24e1543ac5e316
Yaohong Xiang <xiang.yao@northeastern.edu>
2023-09-12 15:34:16 -0400

    change directory and backend dependencies

--- backend/index.js
+++ backend/index.js
@@ -76,7 +76,7 @@ io.on("connection", (socket) => {
 
 
     socket.on("send-msg", (data) => {
-        console.log(data);
+        // console.log(data);
         const sendUserSocket = onlineUsers.get(data.to);
         if(sendUserSocket){
             console.log(data);
